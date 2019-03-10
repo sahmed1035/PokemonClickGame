152cd5045a0b6869a2910ddfac4a3a9fd7d907dd
sahmed <sahmed@alumni.depaul.edu>
2019-03-09 19:24:19 -0600

    Updates

---
+++ precache-manifest.4a863ab2b5f7866a9b6a97a5e9e41e57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cfbce833414cd9966f3",
+    "url": "/PokemonClickGame/static/js/runtime~main.0db9a5ee.js"
+  },
+  {
+    "revision": "0d1887046ccb79cefe74",
+    "url": "/PokemonClickGame/static/js/main.9b89066d.chunk.js"
+  },
+  {
+    "revision": "258e8dcb119c92ff922a",
+    "url": "/PokemonClickGame/static/js/2.2bc2d0a2.chunk.js"
+  },
+  {
+    "revision": "0d1887046ccb79cefe74",
+    "url": "/PokemonClickGame/static/css/main.634ca40e.chunk.css"
+  },
+  {
+    "revision": "b4010899ccf732e7e8ee8aa4bda6c7eb",
+    "url": "/PokemonClickGame/index.html"
+  }
+];
